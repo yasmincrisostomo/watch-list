95cc19946c99c8e31ec47989bf9194624543ec45
yasmin crisostomo <crisostomyasmin@gmail.com>
2022-06-23 17:08:05 -0300

    show

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,5 +1,6 @@
 class List < ApplicationRecord
   validates :name, presence: true, uniqueness: true
+  validates :photo, presence: true
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
   has_one_attached :photo
