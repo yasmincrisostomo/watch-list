d6905448bb9464ac033415206545d6aebfc00f4b
yasmin crisostomo <crisostomyasmin@gmail.com>
2022-06-23 12:37:23 -0300

    delete

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :lists, except: %i[destroy edit update] do
     resources :bookmarks, only: %i[new create]
   end
+  resources :lists, only: :destroy
   resources :bookmarks, :destroy
 end
 
